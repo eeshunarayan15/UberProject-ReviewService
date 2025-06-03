e0fc2f584a9e2acdcd36ba20ec8931486bac4515
Eeshu Narayan <eeshunarayan15@gmail.com>
2025-06-04 01:47:10 +0530

    initial commit & Project Setup

---
+++ src/test/java/com/eeshu/uberreviewservice/UberReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeshu.uberreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
